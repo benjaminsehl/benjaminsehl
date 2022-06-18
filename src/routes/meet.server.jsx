e34a98e099b1752e3db0f284d1cc867aaf6e9779
Benjamin Sehl <ben.sehl@shopify.com>
2022-06-18 13:50:47 -0400

    Adds calendar redirect

---
+++ src/routes/meet.server.jsx
@@ -0,0 +1,5 @@
+export default function Redirect({ response }) {
+  return response.redirect(
+    "https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ3xdrpAVcBjRLhroTPri4H6vSb01yRJmpK6sQsWX_LIiUz4sIq1VnBoW-7M6pgWe9MD9ZkRK6Oz"
+  );
+}

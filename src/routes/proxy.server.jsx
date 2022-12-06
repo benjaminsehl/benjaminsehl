76a396fbe3bdeeca8c57ce553fbfdbb27de0090d
Benjamin Sehl <ben.sehl@shopify.com>
2022-12-06 14:58:52 -0500

    Adds proxy

---
+++ src/routes/proxy.server.jsx
@@ -0,0 +1,12 @@
+export async function api(request, { params }) {
+    // This approach is only supported by Oxygen
+    // const { handle } = params;
+    const response = await fetch("https://checkout.sehl.ca/", {
+      headers: {
+        // It's important to set or forward a User-Agent, otherwise the Online Store might block the request
+        'User-Agent': 'Hydrogen',
+      },
+    });
+    const data = await response.text();
+    return new Response(data, { headers: { "content-type": "text/html" } });
+  }

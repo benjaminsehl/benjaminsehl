80a06dbfbdbe556968cdac997498604f3ef68174
Benjamin Sehl <ben.sehl@shopify.com>
2023-02-15 13:45:21 -0500

    Update proxy route

--- src/routes/proxy.server.jsx
+++ src/routes/proxy.server.jsx
@@ -1,12 +1,15 @@
 export async function api(request, { params }) {
-    // This approach is only supported by Oxygen
-    // const { handle } = params;
-    const response = await fetch("https://checkout.sehl.ca/", {
-      headers: {
-        // It's important to set or forward a User-Agent, otherwise the Online Store might block the request
-        'User-Agent': 'Hydrogen',
-      },
-    });
-    const data = await response.text();
-    return new Response(data, { headers: { "content-type": "text/html" } });
-  }
+  // This approach is only supported by Oxygen
+  // const { handle } = params;
+  const response = await fetch("https://checkout.sehl.ca/", {
+    headers: {
+      // It's important to set or forward a User-Agent, otherwise the Online Store might block the request
+      "User-Agent": "Hydrogen",
+    },
+  });
+  const data = await response.text();
+  return new Response(
+    data.replace('"monorailRegion":"shop_domain"', '"monorailRegion":"global"'),
+    { headers: { "content-type": "text/html" } }
+  );
+}

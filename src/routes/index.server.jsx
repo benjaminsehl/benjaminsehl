9313aa894ff1b3b3dbaebde59427f83a0bfc8f91
Benjamin Sehl <ben.sehl@shopify.com>
2023-03-13 07:47:37 -0400

    Concat sentences as a paragraph.

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -71,16 +71,8 @@ export default function Home() {
     content: {
       readme: {
         text: `I'm Ben Sehl, a Torontonian dad, and a Product Manager at Shopify. 
-
-I've worked in various roles, including design, development, marketing, and strategy.
-
-I co-founded a brand called [Kotn](https://kotn.com) with my wife and oldest friend, and worked on all things digital. 
-
-At Shopify, I'm working on [Hydrogen](https://h2o.shop) as a PM, trying to make it more fun to build websites.
-
-Occasional nights and weekends I also spend working on [Margin](https://margin.global); a premium skincare company I've partnered with my friend Drew on.
-
-Lastly, every once in a while I also push forward on some [side projects](https://incremental.studio).`,
+        
+        I've worked in various roles, including design, development, marketing, and strategy. I co-founded a brand called [Kotn](https://kotn.com) with my wife and oldest friend, and worked on all things digital. At Shopify, I'm working on [Hydrogen](https://h2o.shop) as a PM, trying to make it more fun to build websites. Nights and weekends, I spend time working on [Margin](https://margin.global), a premium skincare company, and push forward on some [side projects](https://incremental.studio).`,
       },
     },
   };
